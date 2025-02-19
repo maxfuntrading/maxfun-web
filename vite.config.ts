2316b4ddbdaef6942283f2edeeca118ebc25153d
leonw111 <leigang@anz.io>
2025-02-19 15:34:56 +0800

    remove console

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import removeConsole from 'vite-plugin-remove-console'
 
 const rootDir = resolve(__dirname);
 const srcDir = resolve(rootDir, 'src');
@@ -9,7 +10,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [react()],
+    plugins: [react(), removeConsole()],
     resolve: {
       alias: {
         '@': srcDir,
